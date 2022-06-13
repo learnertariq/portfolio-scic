436263c26e3e3739b7f6859401a4ebda1e918d64
Md Tariqul Islam <programmertariq@gmail.com>
2022-06-13 13:14:13 +0600

    knocking with emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import contactImg from "../assets/contact.png";
 
 const Contact = () => {
+  const handleKnock = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_7laxqqs",
+        "template_rdh0gfb",
+        e.target,
+        "74fe7E40bGczDm6QT"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div id="contact" className="mt-16">
       <h2 className="text-4xl text-center mb-6">Contact</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
         <img src={contactImg} alt="" />
-        <form className="mx-auto w-96 max-w-full">
+        <form onSubmit={handleKnock} className="mx-auto w-96 max-w-full">
           <div className="form-control w-full max-w-xs mb-3">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
             <input
+              name="name"
               type="text"
               placeholder="Type here"
               className="input input-bordered w-full max-w-xs"
@@ -24,6 +46,7 @@ const Contact = () => {
               <span className="label-text">Email</span>
             </label>
             <input
+              name="email"
               type="email"
               placeholder="Type here"
               className="input input-bordered w-full max-w-xs"
@@ -32,9 +55,10 @@ const Contact = () => {
 
           <div className="form-control w-full max-w-xs mb-3">
             <label className="label">
-              <span className="label-text">Message</span>
+              <span className="label-text">Subject</span>
             </label>
             <input
+              name="subject"
               type="text"
               placeholder="Type here"
               className="input input-bordered w-full max-w-xs"
@@ -45,6 +69,7 @@ const Contact = () => {
               <span className="label-text">Message</span>
             </label>
             <textarea
+              name="message"
               className="textarea textarea-bordered"
               placeholder="Message"
             ></textarea>
